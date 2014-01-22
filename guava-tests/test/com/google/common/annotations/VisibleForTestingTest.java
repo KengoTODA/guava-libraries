0f9aff0c3e099bbe70072c1f4c2df5ac95b403e3
Kengo TODA <toda_k@worksap.co.jp>
2014-01-22 09:26:04 +0800

    make test cases symmetrically

--- guava-tests/test/com/google/common/annotations/VisibleForTestingTest.java
+++ guava-tests/test/com/google/common/annotations/VisibleForTestingTest.java
@@ -1,9 +1,5 @@
 package com.google.common.annotations;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
-
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.Reader;
@@ -12,48 +8,67 @@ import java.lang.reflect.Method;
 
 import junit.framework.TestCase;
 
-import com.google.common.annotations.VisibleForTesting.Visibility;
+import static com.google.common.annotations.VisibleForTesting.Visibility.*;
 
 public class VisibleForTestingTest extends TestCase {
     public void testPrivate() throws SecurityException, NoSuchMethodException {
-        Visibility visibility = Visibility.PRIVATE;
-
-        assertTrue(visibility.allowsAccess(Object.class, Object.class));
+        assertTrue(PRIVATE.allowsAccess(Object.class, Object.class));
 
         Method closeMethod = Writer.class.getMethod("close");
-        assertThat(closeMethod, is(notNullValue()));
-        assertTrue(visibility.allowsAccess(VisibleForTestingTest.class, ExtendedWriter.class));
-        assertTrue(visibility.allowsAccess(ExtendedWriter.class, VisibleForTestingTest.class));
-        assertTrue(visibility.allowsAccess(Writer.class, closeMethod));
-        assertFalse(visibility.allowsAccess(Reader.class, closeMethod));
-        assertFalse(visibility.allowsAccess(Object.class, closeMethod));
+        assertNotNull(closeMethod);
+        assertTrue(PRIVATE.allowsAccess(VisibleForTestingTest.class, ExtendedWriter.class));
+        assertTrue(PRIVATE.allowsAccess(ExtendedWriter.class, VisibleForTestingTest.class));
+        assertTrue(PRIVATE.allowsAccess(Writer.class, closeMethod));
+        assertFalse(PRIVATE.allowsAccess(Reader.class, closeMethod));
+        assertFalse(PRIVATE.allowsAccess(Object.class, closeMethod));
+        assertFalse(PRIVATE.allowsAccess(ExtendedWriter.class, closeMethod));
+
+        Method extendedCloseMethod = ExtendedWriter.class.getMethod("close");
+        assertNotNull(extendedCloseMethod);
+        assertTrue(PRIVATE.allowsAccess(ExtendedWriter.class, extendedCloseMethod));
+        assertFalse(PRIVATE.allowsAccess(Writer.class, extendedCloseMethod));
+        assertFalse(PRIVATE.allowsAccess(Object.class, extendedCloseMethod));
+        assertFalse(PRIVATE.allowsAccess(Reader.class, extendedCloseMethod));
     }
 
     public void testPackage() throws SecurityException, NoSuchMethodException {
-        Visibility visibility = Visibility.PACKAGE;
-
-        assertTrue(visibility.allowsAccess(Object.class, Object.class));
+        assertTrue(PACKAGE.allowsAccess(Object.class, Object.class));
 
         Method closeMethod = Writer.class.getMethod("close");
-        assertThat(closeMethod, is(notNullValue()));
-        assertTrue(visibility.allowsAccess(Writer.class, closeMethod));
-        assertTrue(visibility.allowsAccess(Reader.class, closeMethod));
-        assertFalse(visibility.allowsAccess(Object.class, closeMethod));
+        assertNotNull(closeMethod);
+        assertTrue(PACKAGE.allowsAccess(VisibleForTestingTest.class, ExtendedWriter.class));
+        assertTrue(PACKAGE.allowsAccess(ExtendedWriter.class, VisibleForTestingTest.class));
+        assertTrue(PACKAGE.allowsAccess(Writer.class, closeMethod));
+        assertTrue(PACKAGE.allowsAccess(Reader.class, closeMethod));
+        assertFalse(PACKAGE.allowsAccess(Object.class, closeMethod));
+        assertFalse(PACKAGE.allowsAccess(ExtendedWriter.class, closeMethod));
+
+        Method extendedCloseMethod = ExtendedWriter.class.getMethod("close");
+        assertNotNull(extendedCloseMethod);
+        assertTrue(PACKAGE.allowsAccess(ExtendedWriter.class, extendedCloseMethod));
+        assertFalse(PACKAGE.allowsAccess(Writer.class, extendedCloseMethod));
+        assertFalse(PACKAGE.allowsAccess(Object.class, extendedCloseMethod));
+        assertFalse(PACKAGE.allowsAccess(Reader.class, extendedCloseMethod));
     }
 
     public void testProtected() throws SecurityException, NoSuchMethodException {
-        Visibility visibility = Visibility.PROTECTED;
-
-        assertTrue(visibility.allowsAccess(Object.class, Object.class));
+        assertTrue(PROTECTED.allowsAccess(Object.class, Object.class));
 
         Method closeMethod = Writer.class.getMethod("close");
+        assertNotNull(closeMethod);
+        assertTrue(PROTECTED.allowsAccess(VisibleForTestingTest.class, ExtendedWriter.class));
+        assertTrue(PROTECTED.allowsAccess(ExtendedWriter.class, VisibleForTestingTest.class));
+        assertTrue(PROTECTED.allowsAccess(Writer.class, closeMethod));
+        assertTrue(PROTECTED.allowsAccess(Reader.class, closeMethod));
+        assertFalse(PROTECTED.allowsAccess(Object.class, closeMethod));
+        assertTrue(PROTECTED.allowsAccess(ExtendedWriter.class, closeMethod));
+
         Method extendedCloseMethod = ExtendedWriter.class.getMethod("close");
-        assertThat(extendedCloseMethod, is(notNullValue()));
-        assertFalse(visibility.allowsAccess(Writer.class, extendedCloseMethod));
-        assertFalse(visibility.allowsAccess(Reader.class, extendedCloseMethod));
-        assertTrue(visibility.allowsAccess(ExtendedWriter.class, extendedCloseMethod));
-        assertTrue(visibility.allowsAccess(ExtendedWriter.class, closeMethod));
-        assertFalse(visibility.allowsAccess(Object.class, extendedCloseMethod));
+        assertNotNull(extendedCloseMethod);
+        assertTrue(PROTECTED.allowsAccess(ExtendedWriter.class, extendedCloseMethod));
+        assertFalse(PROTECTED.allowsAccess(Writer.class, extendedCloseMethod));
+        assertFalse(PROTECTED.allowsAccess(Object.class, extendedCloseMethod));
+        assertFalse(PROTECTED.allowsAccess(Reader.class, extendedCloseMethod));
     }
 
     private static final class ExtendedWriter extends BufferedWriter {
